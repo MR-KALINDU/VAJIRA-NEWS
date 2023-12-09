f964fb0626921c6129a5015ab098a429f20915d5
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-12-10 04:34:05 +0530

    📃 ＶＡＪＩＲＡ - ＮＥＷＳ 📃

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const request = require('@cypress/request');
   
  async function news() { 
   
-     let response = await fetch('https://darkapi--technicalhacke4.repl.co/derananews'); 
+     let response = await fetch('https://hirunews.aquaapk-dl.repl.co/api/latest'); 
      let data = await response.json(); 
 let mg =`*${data.title}* 
 ●━━━━━━━━━━━━━━━━━━━━━●  
